e62133ec44f20a586f64183a1519a041eb08b942
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-06-21 17:00:57 -0300

    fix: corrigindo a possibilidade de deletar um usuario nao existente

--- src/modules/users/useCases/deleteUser/deleteUserController.ts
+++ src/modules/users/useCases/deleteUser/deleteUserController.ts
@@ -5,9 +5,13 @@ class DeleteUserController {
     constructor(private deleteUserUseCase: DeleteUserUseCase){}
 
     handle(request: Request, response: Response): Response {
-        const { user_id } = request.params;
-        const delete_user = this.deleteUserUseCase.execute({user_id});
-        return response.status(201).json({delete_user});
+        try{
+            const { user_id } = request.params;
+            const delete_user = this.deleteUserUseCase.execute({user_id});
+            return response.status(201).json({delete_user});
+        }catch(error) {
+            return response.status(400).json({error: error.message});
+        }
     }
 }
 
--- src/modules/users/useCases/deleteUser/deleteUserUseCase.ts
+++ src/modules/users/useCases/deleteUser/deleteUserUseCase.ts
@@ -7,6 +7,12 @@ interface IRequest {
 class DeleteUserUseCase {
     constructor(private usersRepository: UsersRepository){}
     execute({user_id}:IRequest){
+        const existingUser = this.usersRepository.findById(user_id);
+
+        if(!existingUser){
+            throw new Error("Usuário não encontrado!");
+        }
+        
         const user = this.usersRepository.deleteUser(user_id);
         return user;
     }
