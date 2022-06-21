cab818d7165505f6cd338eeca938600301287b5c
Marcos Alexandre <junior.marcos.mjr@hotmail.com>
2022-06-21 17:56:50 -0300

    fix: corrigindo possibilidade de alterar um usuario não existente

--- src/modules/users/useCases/updateUser/updateController.ts
+++ src/modules/users/useCases/updateUser/updateController.ts
@@ -4,9 +4,13 @@ import { UpdateUseCase } from "./updateUseCase";
 class UpdateController {
     constructor(private updateUseCase: UpdateUseCase) {}
     handle(request: Request, response: Response): Response {
-        const { user_id,new_email } = request.params;
-        const update_user = this.updateUseCase.execute({user_id,new_email});
-        return response.status(201).json({update_user});
+        try{
+            const { user_id,new_email } = request.params;
+            const update_user = this.updateUseCase.execute({user_id,new_email});
+            return response.status(201).json({update_user});
+        }catch(error){
+            return response.status(400).json({error: error.message});
+        }
     }
 }
 
